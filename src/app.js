5e223b3d43726e0253fce72aa5b8c1af347bd1e5
Angel Crispin <155650252+AngelCrispin@users.noreply.github.com>
2024-03-12 16:40:21 +0000

    Generador de Excusas en Javascript

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = generateExcuse();
+  document.getElementById("excuse").innerHTML = excuse;
 };
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let wordRandom = function(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+
+  let excuseComplete =
+    wordRandom(who) +
+    " " +
+    wordRandom(action) +
+    " " +
+    wordRandom(what) +
+    " " +
+    wordRandom(when);
+
+  return excuseComplete;
+}
